3d10726e00051052a694357384b8a971e934df92
tomasmark79 <tomas@digitalspace.name>
2025-08-27 18:56:57 +0200

    just Linux fine tune

--- black_hole.cpp
+++ black_hole.cpp
@@ -166,8 +166,8 @@ struct Engine {
     GLuint gridEBO = 0;
     int gridIndexCount = 0;
 
-    int WIDTH = 800;  // Window width
-    int HEIGHT = 600; // Window height
+    int WIDTH = 1920;  // Window width
+    int HEIGHT = 1080; // Window height
     int COMPUTE_WIDTH  = 200;   // Compute resolution width
     int COMPUTE_HEIGHT = 150;  // Compute resolution height
     float width = 100000000000.0f; // Width of the viewport in meters
